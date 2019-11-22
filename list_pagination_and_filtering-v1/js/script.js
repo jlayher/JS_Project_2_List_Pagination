9902988e2516af3a150a4f1f560aaba4c1f8e8a1
jlayher <john.e.layher@gmail.com>
2019-11-22 00:09:18 -0500

    Starting Exceeds Expectations Code
    Removed Treehouse comments.  Added my own comments.  Finished appendPageLinks function.  Made function createSearch to create and append html elements.

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -1,46 +1,19 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
+// Global Variables Created
 const studentList = document.querySelectorAll('.student-item');
 const perPage = 10;
+// Global Variables For Exceeds Expectations Created
+const headerDiv = document.querySelector('.page-header');
+const searchDiv = document.createElement('div');
+const searchInput = document.createElement('input');
+const searchButton = document.createElement('button');
 
 
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
+/* The Function showPage creates a list of students, using the arguments
+list (representing the student list items contained in the gloabl variable
+above) and a page number as the 2nd argument.  This creates a "page" of up to 10
+student list items. */
 const showPage = (list, page) => {
-  /*if we start our buttons with "button 1" then the first start index is 1,
-  but remember that the first li element is at index 0.*/
   const startIndex = (page * perPage) - perPage;
   const endIndex = page * perPage;
   for (let i = 0; i < list.length; i++) {
@@ -52,46 +25,65 @@ const showPage = (list, page) => {
     }
   }
 }
-//test of showPage Function
-showPage(studentList, 1);
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
 
-
+/* The Fucntion appendPageLinks creates a div(pagination), and a ul element
+that stores up to 10 list items (student-items) per page, and creates anchor
+elements for the number of pages needed to access the list items in sets of 10.
+*/
 const appendPageLinks = (list) => {
-  // This likely needs to be Math.ceil(list.length/perPage);
   let maxPages = Math.ceil(list.length / perPage);
   let pageDiv = document.querySelector('.page');
   let paginationDiv = document.createElement('div');
   paginationDiv.className = '.pagination';
-  let ul = document.createElement('ul');
   pageDiv.appendChild(paginationDiv);
+  let ul = document.createElement('ul');
   paginationDiv.appendChild(ul);
-
-
-  /* Does i need to be < or <= numOfPages, and does numOfPages need
-      to be rounded up?
-  */
-  for(i=0; i<= maxPages; i++) {
+  /*This loop creates and appends the links that will function as our "page number"
+    when we call showPage later in this function*/
+  for(i=0; i< maxPages; i++) {
     let li = document.createElement('li');
     ul.appendChild(li);
-    let anchor = document.createElement('a');
-    ul.appendChild(anchor);
-    li.textContent = "" + i +"";
-    anchor.textContent = "" + i + "";
-
-
-    /* add evnet listener to each anchor tag, and each anchor tag should have
-    their text content = the a page number.  In this case, there should be 6
-    buttons
-    */
-
-
+    let a = document.createElement('a');
+    li.appendChild(a);
+    a.textContent = i+1;
+    a.href= "#";
+    let firstAnchor = document.querySelector('a');
+    firstAnchor.className = ".active";
+    let anchorList = document.querySelectorAll('a');
+    for (let j = 0; j < anchorList.length; j++) {
+      a.addEventListener('click', (event) => {
+        showPage(list, event.target.textContent);
+        for (let k = 0; k <= anchorList.length; k++) {
+          anchorList[k].className = 'none';
+        }
+      event.target.className = '.active';
+      });
+    }
   }
 }
 
+// create search component
+
+const createSearch = () => {
+  headerDiv.appendChild(searchDiv);
+  searchDiv.className = "student-search";
+  searchDiv.appendChild(searchInput);
+  searchInput.placeholder = "Search for students...";
+  searchDiv.appendChild(searchButton);
+  searchButton.textContent = "Search For Student!";
+}
+
+
+
+
+
+
+
+
+
+// Calling all Functions
+createSearch();
+showPage(studentList, 1);
 appendPageLinks(studentList);
 
 
