a2823da5076194d2ac5faff924bbe3314b33649d
jlayher <john.e.layher@gmail.com>
2019-11-23 21:19:42 -0500

    Debugging

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -31,6 +31,8 @@ that stores up to 10 list items (student-items) per page, and creates anchor
 elements for the number of pages needed to access the list items in sets of 10.
 */
 const appendPageLinks = (list) => {
+
+  //do I need Math.ceil?
   let maxPages = Math.ceil(list.length / perPage);
   let pageDiv = document.querySelector('.page');
   let paginationDiv = document.createElement('div');
@@ -38,6 +40,11 @@ const appendPageLinks = (list) => {
   pageDiv.appendChild(paginationDiv);
   let ul = document.createElement('ul');
   paginationDiv.appendChild(ul);
+  //attempt to remove/prevent duplicate pagination appendPageLinks when searching
+  const paginationDivRemove = document.querySelector('.pagination');
+  if (document.contains(paginationDivRemove)){
+    paginationDivRemove.remove();
+  }
   /*This loop creates and appends the links that will function as our "page number"
     when we call showPage later in this function*/
   for(i=0; i< maxPages; i++) {
@@ -54,7 +61,9 @@ const appendPageLinks = (list) => {
       a.addEventListener('click', (event) => {
         showPage(list, event.target.textContent);
         for (let k = 0; k <= anchorList.length; k++) {
-          anchorList[k].className = 'none';
+          //attempt at solving anchorList being undefined
+          let anchorList2 = document.querySelectorAll('a');
+          anchorList2[k].className = 'none';
         }
       event.target.className = '.active';
       });
@@ -73,12 +82,48 @@ const createSearch = () => {
   searchButton.textContent = "Search For Student!";
 }
 
+// create search array and run the search.  Call showPage and appendPageLinks
+const searchStudents = (input, list) => {
+  let searchList = [];
+  //attempt to remove repeating li and anchor tags
+  //Removes the "No Results" message when performing search
+  const noResults1 = document.querySelector('.no-results');
+  if (document.contains(noResults1)){
+    noResults1.remove();
+  }
+  //should i be < or <= ?
+  for (let i = 0; i < list.length; i++) {
+    // may not be needed, but preemptively removing the display of all list items before searching
+    list[i].style.display = 'none';
+    if(list[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())){
+      searchList.push(list[i]);
+    }
+  }
+    if (searchList.length === 0) {
+      /*do I need to create a new li, or attach this to the page div, or
+      the student h2?*/
+      let noResults = document.createElement('li');
+      noResults.className = "no-results";
+      searchDiv.appendChild(noResults);
+      noResults.textContent = "No Results";
+    }
+  showPage(searchList, 1);
+  appendPageLinks(searchList);
+};
 
 
+// Add keyup event listener for searchInput, and run searchStudents
+searchInput.addEventListener('keyup', (event) => {
+  let input = searchInput.value;
+  searchStudents(input, studentList);
+});
 
-
-
-
+// Add Click event Listener for Search Button, and run searchStudents
+searchButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  let input = searchInput.value;
+  searchStudents(input, studentList);
+});
 
 
 // Calling all Functions
